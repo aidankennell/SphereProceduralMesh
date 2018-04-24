cacf875f2e707393fcd7135c1416120d05f6de4c
aidankennell <32394698+aidankennell@users.noreply.github.com>
2018-04-24 12:10:23 -0700

    done

---
+++ SphereProceduralMesh/Assets/Scripts/MeshCreator.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshCreator{
+    private List<Vector3> verts = new List<Vector3>();
+    private List<Vector3> normals = new List<Vector3>();
+    private List<Vector2> uvs = new List<Vector2>();
+    private List<int> triangleIndices = new List<int>();
+    private Vector3 center;
+
+    public MeshCreator(Vector3 center)
+    {
+        this.center = center;
+    }
+
+
+    public void BuildTriangle(Vector3 vertex0, Vector3 vertex1, Vector3 vertex2)
+    {
+        int v0Index = verts.Count;
+        int v1Index = verts.Count + 1;
+        int v2Index = verts.Count + 2;
+
+        // Put vertex data into verts array 
+        verts.Add(vertex0);
+        verts.Add(vertex1);
+        verts.Add(vertex2);
+        
+        // Use the same normal for all verts (i.e., a surface normal) 
+        // Could change function signature to pass in 3 normals if needed  
+        normals.Add(findNormal(vertex0));
+        normals.Add(findNormal(vertex1));
+        normals.Add(findNormal(vertex2));
+
+        // Use standard uv coordinates  
+        uvs.Add(new Vector2(0, 0));
+        uvs.Add(new Vector2(0, 1));
+        uvs.Add(new Vector2(1, 1));
+
+        // Add integer pointers to verts into triangles list 
+        triangleIndices.Add(v0Index);
+        triangleIndices.Add(v1Index);
+        triangleIndices.Add(v2Index);
+
+    }
+
+    //public void BuildTriangle(Vector3 vertex0, Vector3 vertex1, Vector3 vertex2)
+    //{
+    //    Vector3 normal = Vector3.Cross(vertex0 - vertex1, vertex2 - vertex1).normalized * -1f;
+    //    BuildTriangle(vertex0, vertex1, vertex2, normal);
+    //}
+
+
+    public Mesh CreateMesh()
+    {
+        Mesh mesh = new Mesh();
+        mesh.vertices = verts.ToArray();
+        mesh.normals = normals.ToArray();
+        mesh.uv = uvs.ToArray();
+        mesh.triangles = triangleIndices.ToArray();
+        return mesh;
+
+    }
+    
+    private void midPoint(int v1, int v2)
+    {
+
+    }
+
+    private void refineASphere()
+    {
+        //foreach()
+    }
+
+
+    private Vector3 findNormal(Vector3 point)
+    {
+        return (center - point).normalized;
+    }
+
+}
---
+++ SphereProceduralMesh/Assets/Scripts/SphereScript.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshFilter))]
+
+public class SphereScript : MonoBehaviour {
+
+    private Vector3 center;
+    private float radius = 1f;
+    private float timeMod = .5f;
+    private float timer = 0;
+    private float scaler = 2f;
+    private bool usingNoise = false;
+
+    private void Update()
+    {
+
+        center = transform.position;
+        MeshFilter meshFilter = this.GetComponent<MeshFilter>();
+        MeshCreator mc = new MeshCreator(center);
+
+        timer += Time.deltaTime * timeMod;
+        if(timer > timeMod * 5)
+        {
+            usingNoise = true;
+        }
+        if(timer > 255)
+        {
+            timer = 0;
+        }
+
+        float noiseValue;
+
+        float t = (radius + Mathf.Sqrt(5f)) / 2f;
+
+        List<Vector3> verts = new List<Vector3>();
+        verts.Add( new Vector3(-radius, t, 0));
+        verts.Add(new Vector3(radius, t, 0));
+        verts.Add(new Vector3(-radius, -t, 0));
+        verts.Add(new Vector3(radius, -t, 0));
+
+
+        verts.Add(new Vector3(0, -radius, t));
+        verts.Add(new Vector3(0, radius, t));
+        verts.Add(new Vector3(0, -radius, -t));
+        verts.Add(new Vector3(0, radius, -t));
+
+
+        verts.Add( new Vector3(t, 0, -radius));
+        verts.Add( new Vector3(t, 0, radius));
+        verts.Add(new Vector3(-t, 0, -radius));
+        verts.Add(new Vector3(-t, 0, radius));
+
+
+
+        if (usingNoise)
+        {
+            for(int i = 0; i < verts.Count; i++)
+            {
+                noiseValue = Perlin.Noise(verts[i].x , verts[i].y, timer);
+                verts[i] += (center - verts[i]).normalized * noiseValue * scaler;
+            }
+            
+        }
+
+        mc.BuildTriangle(verts[5], verts[11], verts[0]);
+        mc.BuildTriangle(verts[1], verts[5], verts[0]);
+        mc.BuildTriangle(verts[7], verts[1], verts[0]);
+        mc.BuildTriangle(verts[10], verts[7], verts[0]);
+        mc.BuildTriangle(verts[11], verts[10], verts[0]);
+
+        mc.BuildTriangle(verts[9], verts[5], verts[1]);
+        mc.BuildTriangle(verts[4], verts[11], verts[5]);
+        mc.BuildTriangle(verts[2], verts[10], verts[11]);
+        mc.BuildTriangle(verts[6], verts[7], verts[10]);
+        mc.BuildTriangle(verts[8], verts[1], verts[7]);
+
+        mc.BuildTriangle(verts[4], verts[9], verts[3]);
+        mc.BuildTriangle(verts[2], verts[4], verts[3]);
+        mc.BuildTriangle(verts[6], verts[2], verts[3]);
+        mc.BuildTriangle(verts[8], verts[6], verts[3]);
+        mc.BuildTriangle(verts[9], verts[8], verts[3]);
+
+        mc.BuildTriangle(verts[5], verts[9], verts[4]);
+        mc.BuildTriangle(verts[11], verts[4], verts[2]);
+        mc.BuildTriangle(verts[10], verts[2], verts[6]);
+        mc.BuildTriangle(verts[7], verts[6], verts[8]);
+        mc.BuildTriangle(verts[1], verts[8], verts[9]);
+
+
+
+        meshFilter.mesh = mc.CreateMesh();
+    }
+}
